ea8ea695596b963e7519f0bfe666f91c436bf00f
Masum <h.m.asrafulmasum@gmail.com>
2024-05-25 18:02:30 +0600

    banner bg image changed

--- client/src/Components/BannerSlider.jsx
+++ client/src/Components/BannerSlider.jsx
@@ -13,7 +13,7 @@ import Button from './Button';
 function BannerSlider() {
     return (
         <div style={{
-            backgroundImage: `url('https://i.postimg.cc/zfMZ8RSC/overhead-view-blank-spiral-notepad-with-italian-pasta-ingredients-against-black-textured-backdrop.jpg')`,
+            backgroundImage: `url('https://i.postimg.cc/3R65B0bS/close-up-person-cooking.jpg')`,
             backgroundSize: 'cover',
             backgroundPosition: 'center',
             width: '100%',
@@ -42,8 +42,8 @@ function BannerSlider() {
                             <h2 className='text-white text-8xl w-2/3 capitalize'>Unlock The Flavors Of Homemade Goodness</h2>
                             <p className='mt-4 mb-10 w-1/2'>Unleash your inner chef with fresh, flavorful creations. Our recipes cater to every palate and occasion, offering step-by-step guides that make cooking a joyful and nourishing experience for both beginners and seasoned cooks.</p>
                             <div className='flex gap-4'>
-                                <Button text="See Recipes" style="border-none bg-primary text-white px-10 text-lg" />
-                                <button className='text-black hover:border-none text-lg px-10 bg-white rounded-lg font-semibold'>Add Recipes</button>
+                                <Button text="See Recipes" style="border-none bg-primary text-white px-12 text-lg" />
+                                <button className='text-black hover:border-none text-lg px-10 bg-white rounded-lg font-semibold hover:text-primary'>Add Recipes</button>
                             </div>
                         </div>
                     </SwiperSlide>
@@ -52,8 +52,8 @@ function BannerSlider() {
                             <h2 className='text-white text-8xl w-2/3 capitalize'>Spice up your life with our recipes</h2>
                             <p className='mt-4 mb-10 w-1/2'>Unleash your inner chef with fresh, flavorful creations. Our recipes cater to every palate and occasion, offering step-by-step guides that make cooking a joyful and nourishing experience for both beginners and seasoned cooks.</p>
                             <div className='flex gap-4'>
-                                <Button text="See Recipes" style="border-none bg-primary text-white px-10 text-lg" />
-                                <button className='text-black hover:border-none text-lg px-10 bg-white rounded-lg font-semibold'>Add Recipes</button>
+                                <Button text="See Recipes" style="border-none bg-primary text-white px-12 text-lg" />
+                                <button className='text-black hover:border-none text-lg px-10 bg-white rounded-lg font-semibold hover:text-primary'>Add Recipes</button>
                             </div>
                         </div>
                     </SwiperSlide>
@@ -62,8 +62,8 @@ function BannerSlider() {
                             <h2 className='text-white text-8xl w-2/3 capitalize'>Where every recipe tells a story</h2>
                             <p className='mt-4 mb-10 w-1/2'>Unleash your inner chef with fresh, flavorful creations. Our recipes cater to every palate and occasion, offering step-by-step guides that make cooking a joyful and nourishing experience for both beginners and seasoned cooks.</p>
                             <div className='flex gap-4'>
-                                <Button text="See Recipes" style="border-none bg-primary text-white px-10 text-lg" />
-                                <button className='text-black hover:border-none text-lg px-10 bg-white rounded-lg font-semibold'>Add Recipes</button>
+                                <Button text="See Recipes" style="border-none bg-primary text-white px-12 text-lg" />
+                                <button className='text-black hover:border-none text-lg px-10 bg-white rounded-lg font-semibold hover:text-primary'>Add Recipes</button>
                             </div>
                         </div>
                     </SwiperSlide>
@@ -72,8 +72,8 @@ function BannerSlider() {
                             <h2 className='text-white text-8xl w-2/3 capitalize'>Fueling your passion for cooking</h2>
                             <p className='mt-4 mb-10 w-1/2'>Unleash your inner chef with fresh, flavorful creations. Our recipes cater to every palate and occasion, offering step-by-step guides that make cooking a joyful and nourishing experience for both beginners and seasoned cooks.</p>
                             <div className='flex gap-4'>
-                                <Button text="See Recipes" style="border-none bg-primary text-white px-10 text-lg" />
-                                <button className='text-black hover:border-none text-lg px-10 bg-white rounded-lg font-semibold'>Add Recipes</button>
+                                <Button text="See Recipes" style="border-none bg-primary text-white px-12 text-lg" />
+                                <button className='text-black hover:border-none text-lg px-10 bg-white rounded-lg font-semibold hover:text-primary'>Add Recipes</button>
                             </div>
                         </div>
                     </SwiperSlide>
